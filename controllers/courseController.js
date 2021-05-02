093632e112096617878973bff31a7a6389e8c2dc
remarka <remarka_51@mail.ru>
2021-05-02 14:40:31 +0300

    add

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,6 +1,7 @@
 const uuid = require('uuid')
 const path = require('path')
-const {Course, Subject, Lesson} = require('../models/models')
+const ApiError = require('../error/ApiError')
+const {Course, Subject, Lesson, Homework, Lesson_video} = require('../models/models')
 
 class courseController {
     async create(req, res) {
@@ -8,7 +9,7 @@ class courseController {
         const {banner} = req.files
         const user = req.user;
         let fileName = uuid.v4() + '.jpg'
-        // banner.mv(path.resolve(__dirname, '..', 'media/image', fileName))
+        banner.mv(path.resolve(__dirname, '..', 'media/image', fileName))
         const course = await Course.create({title, banner: fileName, description, userId: user.id});
         // let subject = {};
         // let lesson = {};
@@ -24,10 +25,44 @@ class courseController {
     }
 
     async getAll(req, res) {
+        const course = await Course.findAll({
+            include: [{
+                model: Subject,
+                include: [{
+                    model: Lesson,
+                    include: [
+                        {
+                            model: Homework
+                        },
+                        {
+                            model: Lesson_video
+                        }
+                    ]
+                }]
+            }]
+        });
+
+        res.json({course})
 
     }
 
-    async getOne(req, res) {
+    async getOne(req, res, next) {
+        const {id} = req.params
+        if (!id) {
+            return next(ApiError.badRequest('Ошибка'))
+        }
+        const course = await Course.findOne({
+            where: {
+                id
+            },
+            include: [{
+                model: Subject,
+                include: [{
+                    model: Lesson
+                }]
+            }]
+        });
+        res.json({course})
 
     }
 }
